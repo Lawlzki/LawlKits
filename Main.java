b9cc8f5ec67dc2b503e60205a5a3fa9b8029d3f6
Lawlzki <SteezyHD09@gmail.com>
2014-03-26 17:26:28 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+public class LawlKits extends JavaPlugin implements Listener {
+
+	public static HashMap<Player, String> classes = new HashMap<Player, String>();
+
+	public static final Logger logger = Logger.getLogger("Minecraft.LawlKits");
+	public static final String loggerPrefix = "[LawlKits] ";
+
+	public static LawlKits mainPlugin;
+
+	public void onDisable() {
+		log("LawlKits is made by Lawlzki!");
+	}
+
+	public void onEnable() {
+		PluginManager pm = getServer().getPluginManager();
+
+		log("LawlKits is made by Lawlzki!");
+
+		mainPlugin = this;
+
+		getCommand("kit").setExecutor(new KitPluginCommand(this));
+	}
+
+public static void help(Player player) {
+		player.sendMessage(ChatColor.GREEN + "=================["
+				+ ChatColor.BLUE + "LawlKits" + ChatColor.GREEN
+				+ "]=================");
+		player.sendMessage(ChatColor.YELLOW + "/kit [kit]" + ChatColor.BLUE
+				+ " - Allows you to choose a kit");
+		player.sendMessage(ChatColor.GRAY
+				+ "   (Aliases: kits, class, classes)");
+		player.sendMessage(ChatColor.GRAY
+				+ "   (Available Kits: Tim, Fun)");
+		player.sendMessage(ChatColor.GREEN
+				+ "========================================");
+	}
+}
