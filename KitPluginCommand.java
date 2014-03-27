70116d0710456628b33c4ca19b6baadb8c77c78c
Lawlzki <SteezyHD09@gmail.com>
2014-03-27 16:44:18 +0000

    Update KitPluginCommand.java

--- KitPluginCommand.java
+++ KitPluginCommand.java
@@ -33,13 +33,32 @@ public class KitPluginCommand implements CommandExecutor {
 							PlayerUtil.refreshPlayer(player);
 
 							inv.addItem(new ItemStack(Material.WOOD_AXE, 1));
+							ItemMeta wandMeta = wand.getItemMeta();
+							wandMeta.setDisplayName(ChatColor.??? + "Wand");
+							wand.setItemMeta(wandMeta);
 
 							player.addPotionEffect(new PotionEffect(
 									PotionEffectType.SLOW, 1000000, 01));
 							player.addPotionEffect(new PotionEffect(
 									PotionEffectType.INCREASE_DAMAGE, 1000000,
 									1));
-						} else {
-							LawlKits.errMsg(player,
-									"You do not have permission for kit: Tim");
 									
+						} else if (cmd.getName().equalsIgnoreCase("LawlKits")) {
+							
+							Inventory inv = Bukkit.createInventory(null, 5,
+							ChatColor.AQUA + "LawlKits");
+
+							ItemStack tim = new ItemStack(397, 1, (short) 3);
+							ItemMeta timMeta = tim.getItemMeta();
+							timMeta.setOwner("Lawlzki");
+							timMeta.setDisplayName(ChatColor.GOLD + "Tim");
+							List<String> timLore = Arrays.asList(ChatColor.GREEN
+									+ "The best kit ever!");
+							timMeta.setLore(timLore);
+							tim.setItemMeta(timMeta);
+									
+							inv.setItem(0, tim);
+							
+							player.playSound(player.getLocation(), Sound.BAT_TAKEOFF,
+									2F, 1F);
+							player.openInventory(inv);
